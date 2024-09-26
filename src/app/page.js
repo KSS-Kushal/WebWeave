1916a586586f7bab3cb6804b22b49cc8223151e5
Kushal Ghosh <kushalghosh075@gmail.com>
2024-09-26 21:53:46 +0530

    fix

--- src/app/page.js
+++ src/app/page.js
@@ -20,38 +20,39 @@ export default function Home() {
       </div>
       <div className="flex  justify-between items-center  px-7 py-10 bg-gray-300 w-full flex-col md:flex-row gap-3 mt-80 md:mt-52">
         <div className="w-full md:w-1/2 flex justify-center items-center">
-          <p className=" text-xl font-bold text-gray-500">
-            Our Mission
-            </p>
+          <p className=" text-xl font-bold text-gray-500">Our Mission</p>
         </div>
         <div className="w-full md:w-1/2 flex flex-col justify-center items-center ">
           <p className="text-xl font-bold text-gray-800 text-center ">
-        At WebWeave Creations, our mission is to empower small businesses by providing innovative and affordable web solutions. We believe that every business deserves a strong online presence, and we are committed to delivering high-quality websites that are tailored to meet the unique needs of our clients.
+            At WebWeave Creations, our mission is to empower small businesses by
+            providing innovative and affordable web solutions. We believe that
+            every business deserves a strong online presence, and we are
+            committed to delivering high-quality websites that are tailored to
+            meet the unique needs of our clients.
           </p>
           <p className="text-xl font-bold text-gray-800 text-center ">
             Our goals include:
           </p>
           <ul className="list-disc list-inside text-gray-800 ">
             <li>
-              <span className="font-semibold">
-                Empowerment:</span> 
-              Helping businesses thrive in the digital landscape through effective online strategies.
+              <span className="font-semibold">Empowerment:</span>
+              Helping businesses thrive in the digital landscape through
+              effective online strategies.
             </li>
             <li>
-              <span className="font-semibold">
-                Innovation:</span>
-              Utilizing the latest technologies and design trends to create visually appealing and functional websites.
+              <span className="font-semibold">Innovation:</span>
+              Utilizing the latest technologies and design trends to create
+              visually appealing and functional websites.
             </li>
             <li>
-              <span className="font-semibold">
-                
-            Customer-Centric Approach: </span>
-              Prioritizing our clients’ needs and building long-lasting relationships based on trust and satisfaction.
+              <span className="font-semibold">Customer-Centric Approach: </span>
+              Prioritizing our clients’ needs and building long-lasting
+              relationships based on trust and satisfaction.
             </li>
           </ul>
         </div>
       </div>
-      
+
       <div className="my-20 flex flex-col gap-10 w-[90%] md:w-4/5 2xl:w-3/4 mx-auto">
         <p className="text-2xl font-bold text-center text-gray-800">
           Our Services
@@ -76,21 +77,15 @@ export default function Home() {
         <p className="text-4xl font bold">web weave</p>
         <div className="flex justify-between items-center gap-5 flex-col md:flex-row">
           <div className="flex-1 rounded-xl">
-            <img
-              className="w-full rounded-xl "
-              src="panchali.png"
-            />
+            <img className="w-full rounded-xl " src="/assets/panchali.png" width="400px" height="400px" />
+          </div>
+          <div className="flex-1 rounded-xl">
+            <img className="w-full rounded-xl" src="/assets/veda.png" width="400px" height="400px" />
           </div>
           <div className="flex-1 rounded-xl">
             <img
               className="w-full rounded-xl"
-              src="veda.png"
-            />
-          </div>
-          <div className="flex-1 rounded-xl">
-            <img
-              className="w-full rounded-xl"
-              src="https://www.healthkart.com/connect/wp-content/uploads/2021/07/Header-10.jpg"
+              src="/assets/gym.png"
             />
           </div>
         </div>
@@ -98,7 +93,7 @@ export default function Home() {
 
       <div className="flex flex-col justify-center items-center gap-5 w-[90%] md:w-4/5 2xl:w-3/4 mx-auto my-20">
         <p className="text-4xl font bold">Review</p>
-        <div className="flex  justify-center items-center gap-7 overflow-x-scroll w-full ">
+        <div className="flex justify-center items-center gap-7 overflow-x-scroll w-full ">
           <ReviewCard />
           <ReviewCard />
           <ReviewCard />
